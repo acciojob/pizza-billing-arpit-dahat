6b1d973e24075edc33085ea7fead1d937ea6d5a4
Arpit <arpitdahat.ad@gmail.com>
2022-12-19 17:34:42 +0530

    code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,12 +5,12 @@ public class Main {
     DeluxePizza dp = new DeluxePizza(true);
     System.out.println(dp.getPrice());
     dp.addTakeaway();
-    dp.addcheesePrice();
+    dp.addExtraCheese();
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
     Pizza p = new Pizza(true);
-    p.addcheesePrice();
+    p.addExtraCheese();
     p.addExtraToppings();
     p.addTakeaway();
     System.out.println(p.getBill());
